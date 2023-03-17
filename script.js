9964523bfd98fbd97dd05bf5ed31ca3ecef8a613
vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-03-17 15:53:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let Name = document.getElementById("name").value;
+let age = document.getElementById("age").value;
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  //   if (Name === "" || isNaN(age)) {
+  //     alert("inputs cannot be empty");
+  //     return;
+  //   }
+
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve(`Welcome, ${Name}.You can vote. `);
+      } else {
+        reject(`ohh sorry ${Name}.You aren't old enough`);
+      }
+    }, 4000);
+  });
+
+  promise
+    .then((message) => {
+      alert(message);
+    })
+    .catch((message) => {
+      alert(message);
+    });
+});
